26fe139e0d600bb306af4cc00ce713417b744b8c
JoshT <josh.tymburski@uleth.ca>
2017-01-20 12:58:27 -0700

    More work on 10415

--- Exercise10415/main.cc
+++ Exercise10415/main.cc
@@ -15,10 +15,23 @@ int main(int argc, char** argv)
                                       {1, 1, 1, 1, 0, 0, 1, 0, 0, 0},
                                       {1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
                                       {1, 1, 0, 0, 0, 0, 0, 0, 0, 0}};
+    const char notes[16] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C', 'D', 'E', 'F', 'G', 'A', 'B'};
+
     int tableTrackingPressed[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     int numOfTestCases;
 
     std::cin >> numOfTestCases;
-    
+
+    for (int i = 0; i < numOfTestCases; ++i)
+        while (true)
+        {
+            char note;
+            std::cin >> note;
+
+            if (note == '\n')
+                break;
+
+        }
+
     return 0;
 }
