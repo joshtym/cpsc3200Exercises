bdd5d066f5315eaf5e9a60dbc94dbd80bfa6d7e1
JoshT <josh.tymburski@uleth.ca>
2017-01-20 23:17:08 -0700

    Finished exercise 10415

--- Exercise10415/main.cc
+++ Exercise10415/main.cc
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv)
 {
-    const int tableFingers[16][10] = {{0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
+    const int tableFingers[14][10] = {{0, 1, 1, 1, 0, 0, 1, 1, 1, 1},
                                       {0, 1, 1, 1, 0, 0, 1, 1, 1, 0},
                                       {0, 1, 1, 1, 0, 0, 1, 1, 0, 0},
                                       {0, 1, 1, 1, 0, 0, 1, 0, 0, 0},
@@ -14,24 +15,61 @@ int main(int argc, char** argv)
                                       {1, 1, 1, 1, 0, 0, 1, 1, 0, 0},
                                       {1, 1, 1, 1, 0, 0, 1, 0, 0, 0},
                                       {1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+                                      {1, 1, 1, 0, 0, 0, 0, 0, 0, 0},
                                       {1, 1, 0, 0, 0, 0, 0, 0, 0, 0}};
-    const char notes[16] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'C', 'D', 'E', 'F', 'G', 'A', 'B'};
 
-    int tableTrackingPressed[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    std::string notes = "cdefgabCDEFGAB";
+
     int numOfTestCases;
+    char note, previousNote;
+    std::string tempString;
 
     std::cin >> numOfTestCases;
 
+    std::getline(std::cin, tempString, '\n');
+
     for (int i = 0; i < numOfTestCases; ++i)
-        while (true)
+    {
+        int tableTrackingPressed[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        std::string song;
+        bool isFirstNote = true;
+
+        std::getline(std::cin, song, '\n');
+
+        for (int j = 0; j < song.length(); ++j)
         {
-            char note;
-            std::cin >> note;
+            previousNote = note;
+            note = song[j];
 
-            if (note == '\n')
-                break;
+            if (!(isFirstNote))
+            {
+                int noteIndex = notes.find(note);
+                int previousNoteIndex = notes.find(previousNote);
 
+                for (int k = 0; k < 10; ++k)
+                {
+                    if (tableFingers[noteIndex][k] == 1 && tableFingers[previousNoteIndex][k] == 0)
+                        tableTrackingPressed[k] += 1;
+                }
+            }
+            else
+            {
+                isFirstNote = false;
+
+                int noteIndex = notes.find(note);
+
+                for (int k = 0; k < 10; ++k)
+                    if (tableFingers[noteIndex][k] == 1)
+                        tableTrackingPressed[k] += 1;
+            }
         }
+
+        for (int j = 0; j < 10; ++j)
+            if (j != 9)
+                std::cout << tableTrackingPressed[j] << " ";
+            else
+                std::cout << tableTrackingPressed[j] << std::endl;
+    }
 
     return 0;
 }
