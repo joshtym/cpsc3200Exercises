f1684be83ab5e6d8a66263029a54c56d088ae12e
JoshT <josh.tymburski@uleth.ca>
2017-01-24 16:34:07 -0700

    Working minus the wraparound to 0

--- Exercise11947/main.cc
+++ Exercise11947/main.cc
@@ -1,12 +1,13 @@
 #include "date.cc"
 #include <string>
+#include <iomanip>
 
 int main(int argc, char** argv)
 {
     std::string zodiacValues[13][2] = {{"0", "0"}, {"capicorn", "20"}, {"aquarius", "19"}, {"pisces", "20"}, 
                                                    {"aries", "20"}, {"taurus", "21"}, {"gemini", "21"}, 
                                                    {"cancer", "22"}, {"leo", "21"}, {"virgo", "23"}, 
-                                                   {"libra", "23"}, {"scorpio", "22"}, {"sagittaarius", "22"}};
+                                                   {"libra", "23"}, {"scorpio", "22"}, {"sagittarius", "22"}};
 
     int numOfInputs;
     int counter = 1;
@@ -25,7 +26,7 @@ int main(int argc, char** argv)
         if ((date.dd) > std::stoi(zodiacValues[date.mm][1]))
             std::cout << counter++ << " " << date.mm << "/" << date.dd << "/" << date.yyyy << " " << zodiacValues[(date.mm + 1) % 13][0] << std::endl;
         else
-            std::cout << counter++ << " " << date.mm << "/" << date.dd << "/" << date.yyyy << " " << zodiacValues[date.mm % 13][0] << std::endl;
+            std::cout << std::setfill('0') << counter++ << " " << std::setw(2) << date.mm << "/" << std::setw(2) << date.dd << "/" << date.yyyy << " " << zodiacValues[date.mm % 13][0] << std::endl;
     }
 
     return 0;
