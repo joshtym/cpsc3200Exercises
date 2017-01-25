75d8ec2ba37bc0a54fdd6443acc93da1908d0fc4
JoshT <josh.tymburski@uleth.ca>
2017-01-25 15:21:41 -0700

    Commented Exercise10415

--- Exercise10415/main.cc
+++ Exercise10415/main.cc
@@ -1,3 +1,12 @@
+/*
+ * Solution to Exercise 10415 - Eb Alto Saxophone Player. Programmed
+ * using a table driven method. Every note with its corresponding fingering
+ * is set into an integer table with the index denoting the note from
+ * "c" to "B". To find the number of note presses, we figure out how many
+ * times we shift from a 0 to a 1. Observe that we do need to use the
+ *
+ * Author : Joshua Tymburski
+*/
 #include <iostream>
 #include <string>
 
@@ -22,20 +31,34 @@ int main(int argc, char** argv)
 
     int numOfTestCases;
     char note, previousNote;
-    std::string tempString;
 
     std::cin >> numOfTestCases;
 
-    std::getline(std::cin, tempString, '\n');
+    /*
+     * Since we will be using getline in the for loop, we need
+     * to ignore the newline character after reading in the
+     * test case numbers
+    */
+    std::cin.ignore(1, '\n');
 
     for (int i = 0; i < numOfTestCases; ++i)
     {
+        /*
+         * Table which keeps track of number of different presses for
+         * each of the 10 fingers
+        */
         int tableTrackingPressed[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         std::string song;
         bool isFirstNote = true;
 
         std::getline(std::cin, song, '\n');
 
+        /*
+         * Use the index of the song to figure out the current note, then
+         * determine button presses between this note and the previous note
+         * if it is not the first note. Else, we just add all the first note presses
+         * to our table tracking our presses
+        */
         for (int j = 0; j < song.length(); ++j)
         {
             previousNote = note;
@@ -47,10 +70,8 @@ int main(int argc, char** argv)
                 int previousNoteIndex = notes.find(previousNote);
 
                 for (int k = 0; k < 10; ++k)
-                {
                     if (tableFingers[noteIndex][k] == 1 && tableFingers[previousNoteIndex][k] == 0)
-                        tableTrackingPressed[k] += 1;
-                }
+                        tableTrackingPressed[k]++;
             }
             else
             {
@@ -60,10 +81,14 @@ int main(int argc, char** argv)
 
                 for (int k = 0; k < 10; ++k)
                     if (tableFingers[noteIndex][k] == 1)
-                        tableTrackingPressed[k] += 1;
+                        tableTrackingPressed[k]++;
             }
         }
 
+        /*
+         * Only newline once we've printed out all of the table
+         * tracked presses
+        */
         for (int j = 0; j < 10; ++j)
             if (j != 9)
                 std::cout << tableTrackingPressed[j] << " ";
