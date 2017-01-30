50e7288387a3287c2eb3c647c0eb082c45fbb0b1
JoshT <josh.tymburski@uleth.ca>
2017-01-30 00:52:24 -0700

    Actually added beginnings of main

---
+++ Exercise793/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+   int runTimes;
+
+   std::cin >> runTimes;
+
+   for (int i = 0; i < runTimes; ++i)
+   {
+      int numOfComputers;
+      std::cin >> numOfComputers;
+   }
+
+   return 0;
+}
