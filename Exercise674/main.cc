52644d589e3b5802ba8f07b2fd537367bcf0787a
JoshT <josh.tymburski@uleth.ca>
2017-03-02 11:04:07 -0700

    more of the stuffs

--- Exercise674/main.cc
+++ Exercise674/main.cc
@@ -1,17 +1,29 @@
+/*
+ * Solution to exercise 674 from UVA. Uses the bottom up approach
+ * to solve through iteration. A top-down approach should be possible,
+ * but probably more painful.
+ *
+ * Also: https://youtu.be/M0It7Mq-si4
+ *
+ * Author: Joshua Tymburski
+*/
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-   int amount;
+   /*
+    * We can pre-compute all the solutions since there are less then 10000 of them.
+    * Base case has one solution, and use bottom up for rest. Do pennies, followed,
+    * by nickels, etc. etc
+   */
    int coins[5] = {1,5,10,25,50};
    int numOfSolutions[8000] = {0};
-
    numOfSolutions[0] = 1;
-
    for (int i = 0; i < 5; ++i)
       for (int j = 0; j < 7949; ++j)
          numOfSolutions[j + coins[i]] += numOfSolutions[j];
 
+   int amount;
    while (std::cin >> amount)
       std::cout << numOfSolutions[amount] << std::endl;
    
---
+++ Exercise674/main2.cc
@@ -0,0 +1,55 @@
+/*
+ * Solution to UVA Exercise 674 using top-down approach. We start
+ * by getting the dollar value and proceed by starting at index 0
+ * of our set of available coins. By ordering from largest to smallest,
+ * we avoid duplication solutions. We store these solutions as we calculate
+ * them to avoid extra calculations. Note that the allSolutions matrix stores the number of solutions for
+ * a money value using the coins from the inxex to the end. So, allSolutions[300][3]
+ * has all the solutions to make the dollar value 300 using coins from index 3 of setOfCoins
+ * to index 4 (IE, 5 and 1). 
+ *
+ * Author: Joshua Tymburski
+*/
+#include <iostream>
+
+int waysToMakeMoneyValue(int,int);
+
+int setOfCoins[] = {50, 25, 10, 5, 1};
+int allSolutions[8000][5];
+
+int main(int argc, char** argv)
+{
+   int dollarValue;
+
+   while (std::cin >> dollarValue)
+      std::cout << waysToMakeMoneyValue(dollarValue,0) << std::endl;
+
+   return 0;     
+}
+
+int waysToMakeMoneyValue(int moneyLeft, int startingIndex)
+{
+   /*
+    * Clearly, if we have calculated the value or if we have no money left
+    * return the appropriate values
+   */
+   if (moneyLeft == 0)
+      return 1;
+   if (allSolutions[moneyLeft][startingIndex] != 0)
+      return allSolutions[moneyLeft][startingIndex];
+
+   int numberOfWays = 0;
+
+   /*
+    * Interesting recursive step. We start with the largest coin index and
+    * recurse down trying all possible solutions. So, for example, 11 would
+    * try 10,1  then 5,5,1, then 5,1,1,1,1,1,1,then eleven ones
+   */
+   for (int i = startingIndex; i < 5; ++i)
+      if (moneyLeft >= setOfCoins[i])
+         numberOfWays += waysToMakeMoneyValue(moneyLeft - setOfCoins[i],i);
+
+   allSolutions[moneyLeft][startingIndex]  = numberOfWays;
+
+   return numberOfWays;
+}
