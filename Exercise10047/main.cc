f2184c0d51d57af28d19f6f726320c1b2a3d42ae
JoshT <josh.tymburski@uleth.ca>
2017-03-09 14:52:02 -0700

    Fixed and commented 10047

--- Exercise10047/main.cc
+++ Exercise10047/main.cc
@@ -1,3 +1,15 @@
+/*
+ * Solution to UVA Exercise 10047. Uses BFS to expand from starting
+ * target square to end target square. If it reaches the square, it
+ * returns the time taken otherwise, not reachable. Note that each 
+ * square has 3 neighbours : the
+ * direction you can move forward square, turning left, and turning right,
+ * so this is how we expand. We keep details of these in a struct denoted
+ * cell details below. Note that 0 = North and 0 = Green with each successive
+ * incrementation iterating through directions
+ *
+ * Author: Joshua Tymburski
+*/
 #include <iostream>
 #include <queue>
 
@@ -21,10 +33,13 @@ int main(int argc, char** argv)
 
    while (std::cin >> rows >> columns && rows != 0)
    {
+      /*
+       * Dumb semantics for presentation........
+      */
       if (cases != 1)
-         std::cout << std::endl << "Cases #" << cases++ << std::endl;
+         std::cout << std::endl << "Case #" << cases++ << std::endl;
       else
-         std::cout << "Cases #" << cases++ << std::endl;
+         std::cout << "Case #" << cases++ << std::endl;
 
       cellDetails start, end;
 
@@ -56,7 +71,7 @@ int main(int argc, char** argv)
       if (duration < 0)
          std::cout << "destination not reachable" << std::endl;
       else
-         std::cout << "minimum time = " << duration << " seconds" << std::endl;
+         std::cout << "minimum time = " << duration << " sec" << std::endl;
    }
 
    return 0;
@@ -64,10 +79,18 @@ int main(int argc, char** argv)
 
 int timeToEnd(cellDetails start, cellDetails end)
 {
+   /*
+    * The visited quad data is [direction][colour][x][y]
+   */
    bool visited[4][5][25][25] = {false};
+   const int rowDirection[] = {-1,0,1,0};
+   const int columnDirection[] = {0,1,0,-1};
 
    cellDetails current, nextState;
-   int turnRight, turnLeft;
+   
+   /*
+    * Do the normal BFS Jazz with constraints to the current problem
+   */
    std::queue<cellDetails> bfsQueue;
    visited[start.direction][start.colour][start.row][start.column] = true;
    bfsQueue.push(start);
@@ -77,9 +100,18 @@ int timeToEnd(cellDetails start, cellDetails end)
       current = bfsQueue.front();
       bfsQueue.pop();
 
+      /*
+       * We end if we've landed on our target square with green as the colour
+      */
       if (current.row == end.row && current.column == end.column && current.colour == 0)
          return current.timeTaken;
 
+      /*
+       * Create directions and check for visitation of the neighbours for when
+       * you turn left and right. Do normal BFS jazz and 'visit' them by adding
+       * them to the queue as well as incrementing our time taken to get there
+      */
+      int turnRight, turnLeft;
       turnLeft = (current.direction + 3) % 4;
       turnRight = (current.direction + 1)  % 4;
 
@@ -101,52 +133,30 @@ int timeToEnd(cellDetails start, cellDetails end)
          bfsQueue.push(nextState);
       }
 
+      /*
+       * Similar to turning, we do the same for actually moving. Check if we can legitimally move
+       * to the square we are facing and if so, move there (if not already visited). Increment
+       * time taken AND colour by one since we actually moved this time
+      */
       bool canMove = true;
-      int rowDir, columnDir;
+      int newRow = current.row + rowDirection[current.direction];
+      int newColumn = current.column + columnDirection[current.direction];
 
-      switch (current.direction)
-      {
-         case 0:
-            rowDir = -1;
-            columnDir = 0;
-            if (current.row == 0 || grid[current.row - 1][current.column] == '#')
-               canMove = false;
-            break;
-         case 1:
-            rowDir = 0;
-            columnDir = 1;
-            if (current.column + 1 == columns  || grid[current.row][current.column + 1] == '#')
-               canMove = false;
-            break;
-         case 2:
-            rowDir = 1;
-            columnDir = 0;
-            if (current.row + 1 == rows || grid[current.row + 1][current.column] == '#')
-               canMove = false;
-            break;
-         case 3:
-            rowDir = 0;
-            columnDir = -1;
-            if (current.column == 0 || grid[current.row][current.column - 1] == '#')
-               canMove = false;
-            break;
-         default:
-            break;
-      }
+      if (newRow < 0 || newRow == rows || newColumn < 0 || newColumn == columns || grid[newRow][newColumn] == '#')
+         canMove = false;
 
-      if (canMove && !(visited[current.direction][current.colour + 1 % 5][current.row + rowDir][current.column + columnDir]))
+      if (canMove && !(visited[current.direction][(current.colour + 1) % 5][newRow][newColumn]))
       {
-         visited[current.direction][current.colour + 1 % 5][current.row + rowDir][current.column + columnDir] = true;
+         visited[current.direction][(current.colour + 1) % 5][newRow][newColumn] = true;
          nextState = current;
          nextState.colour = (current.colour + 1) % 5;
-         nextState.row += rowDir;
-         nextState.column += columnDir;
+         nextState.row = newRow;
+         nextState.column = newColumn;
          nextState.timeTaken++;
          bfsQueue.push(nextState);
       }
 
    }
-
 
    return -1;
 }
