4e3cb26d4c0e9809a7ead0f453926e391dd95635
JoshT <josh.tymburski@uleth.ca>
2017-03-13 10:33:09 -0600

    Attempted progress on some project problems

---
+++ Project/1999Regionals/main.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+   int cases;
+   int listOfReceptacles[100];
+
+   std::cin >> cases;
+
+   for (int i = 0; i < cases; ++i)
+   {
+      int receptacles;
+      std::cin >> receptacles;
+
+      for (int j = 0; j < receptacles; ++j)
+         std::cout << "DERP" << std::endl;
+
+   }
+
+   return 0;
+}
